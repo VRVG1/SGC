43a274ed158951c2b7fc41e5058c87165121bbfb
Doncamatic1117 <dr.goku45@gmail.com>
2022-05-30 20:25:18 -0500

    Cambios a url de reportes

--- SGC/reportes/urls.py
+++ SGC/reportes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import ReportesView, CreateReportesView, GeneranView, CrearGeneran, borrarReporte, updateReporte, CreateAlojanView, alojanFromView, OnlySaveReportesView, EnviarGeneran, GetGeneranUser
-from .views import GetReporte
+from .views import GetReporte, AlojanView
 urlpatterns = [
     path('reportes', ReportesView.as_view()),
     path('create-reporte', CreateReportesView.as_view()),
     path('generaciones', GeneranView.as_view()),
+    path('alojan', AlojanView.as_view()),
     path('create-alojan', CreateAlojanView.as_view()),
     path('create-gen/<int:pk>', CrearGeneran),
     path('delete-reporte/<int:pk>', borrarReporte),
